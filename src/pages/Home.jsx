ae76084c76584d925b777fabd9bcd683000468b7
stellaragencyai <stellaragencyai@gmail.com>
2026-04-21 06:40:03 -0700

    Restore 8-system section to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Hero from "../components/landing/Hero";
 import TrustBar from "../components/landing/TrustBar";
 import Industries from "../components/landing/Industries";
 import ProblemSolution from "../components/landing/ProblemSolution.jsx";
-import HowItWorks from "../components/landing/HowItWorks";
 import Testimonials from "../components/landing/Testimonials";
 import CoreOffer from "../components/landing/CoreOffer";
 import FAQ from "../components/landing/FAQ";
@@ -62,7 +61,7 @@ export default function Home() {
           <Testimonials />
         </section>
         <ProblemSolution />
-        <HowItWorks id="how-it-works-section" />
+        <CoreOffer />
         <Pricing />
         <FAQ />
         <FinalCTA />
