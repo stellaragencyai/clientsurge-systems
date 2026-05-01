70718a78f51dbb37fb63a1134acda7d74aa22b3b
stellaragencyai <stellaragencyai@gmail.com>
2026-05-01 11:17:24 -0700

    fix: Guarantee grid - cols-2 on sm/md, cols-4 on lg to prevent cramped layout

--- src/components/landing/Guarantee.jsx
+++ src/components/landing/Guarantee.jsx
@@ -39,7 +39,7 @@ export default function Guarantee() {
         </div>
 
         {/* Guarantee Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {guarantees.map((g, i) => {
             const Icon = g.icon;
             return (
