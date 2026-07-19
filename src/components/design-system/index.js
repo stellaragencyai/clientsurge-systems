1c2a4c9011ebf48847099c4cee663fe44673225a
stellaragencyai <stellaragencyai@gmail.com>
2026-07-18 18:12:30 -0700

    Export ClientSurge pricing primitives

--- src/components/design-system/index.js
+++ src/components/design-system/index.js
@@ -1,6 +1,12 @@
 export { default as CSAppShell } from "./CSAppShell";
 export { default as CSAuthSurface } from "./CSAuthSurface";
 export {
+  CSCommerceButton,
+  CSPopularBadge,
+  CSPricingCard,
+  CSPricingGrid,
+} from "./CSPricingPrimitives";
+export {
   CSAlert,
   CSButton,
   CSCard,
