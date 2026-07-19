f924d3dd18fd37bfe1b8983a97b6446f6a286e4f
stellaragencyai <stellaragencyai@gmail.com>
2026-07-18 17:24:44 -0700

    feat: export ClientSurge OS design-system components

---
+++ src/components/design-system/index.js
@@ -0,0 +1,13 @@
+export { default as CSAppShell } from "./CSAppShell";
+export {
+  CSAlert,
+  CSButton,
+  CSCard,
+  CSEmptyState,
+  CSField,
+  CSMetricCard,
+  CSPageHeader,
+  CSProgressSteps,
+  CSSkeleton,
+  CSStatusBadge,
+} from "./CSProductPrimitives";
